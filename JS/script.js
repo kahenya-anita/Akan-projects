c3d0416ff0367694d98f5d0d0a8748a0aea666a0
santa Claus <santa@northpole.com>
2020-04-03 01:19:16 +0300

    Changes in script.js code

--- JS/script.js
+++ JS/script.js
@@ -1,68 +1,77 @@
-document.getElementById("submit").addEventListener("click",function(event){
-    event.preventDefault()
-  })
-  function calc(){ 
-    var dd = document.getElementById("dd").value; //get value of date inputed 
-    var mm = document.getElementById("mm").value; //get value of month inputed 
-    var yy = document.getElementById("yy").value; //get value of year inputed
-    var male = document.getElementById("male").checked; //returns true if male button is selected and false if unselected
-    var female = document.getElementById("female").checked; // returns true if female button is selected and false if unselected
-    var days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var femaleNames = ["Akosua","Adwoa","Abenaa", "Akua","Yaa","Yaa","Afua","Ama"];
-    var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var dates = (dd + '/' + mm +'/' + yy);
-    var birthday = new Date(dates);
-    var DOB = birthDay.getDate();
-    if (dd <= 0 || dd > 31){ //ensures user enters a date and validates date entered for months with 31 days
-        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error 
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "30%"
-      }
-    else if ((mm ==="September" || mm ==="April" || mm ==="June" || mm ==="November") && (dd <= 0 || dd > 30)){ //ensures user enters a date and validates date entered for months with 30 days
-        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error 
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "30%"
-      }
-    else if(mm === "------"){ //ensures user selects a month
-        document.getElementById("error").innerText = "Please select a month!" //shows error message by replacing the HTML content of id=error
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "40%"
-      }
-    else if(yy <= 0 || yy > 2020) { //ensures user enters a year and the year entered is valid and doesn't exceed 2020
-        document.getElementById("error").innerText = "Please enter a valid year!" //shows error message by replacing the HTML content of id=error
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "40%"
-      }
-    else if( (mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4) ){ //sets limit for valid date for the month of February in a leap year to 29
-        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "40%"
-      }
-    else if( (mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4) ){ //sets limit for valid date for the month of February in a non-leap year to 28 
-        document.getElementById("error").innerText = "Please enter a valid date!" //shows error message by replacing the HTML content of id=error
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "40%"
-      }
-    else if (male === true) { //styling of results for a male user
-        document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" +  "Your Akan name is " + maleNames[dayOfTheWeek] + "!" //shows result by replacing the  HTML content in the id=result
-        document.getElementById("result").style.color = "purple"
-        document.getElementById("result").style.padding = "9% 22% 13% 22%"
-        document.getElementById("result").style.fontSize = "35px"
-        document.getElementById("result").style.fontFamily = "'Kalam', cursive"
-        document.getElementById("result").style.textAlign = "center"
-      }
-    else if (female === true) { //styling results of a female user
-        document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!" //shows result by replacing the HTML content in the id=result
-        document.getElementById("result").style.color = "maroon"
-        document.getElementById("result").style.padding = "9% 22% 13% 22%"
-        document.getElementById("result").style.fontSize = "35px"
-        document.getElementById("result").style.fontFamily = "'Kalam', cursive"
-        document.getElementById("result").style.textAlign = "center"
-      }
-    else if(( female === false) && ( male === false)){ //ensures user selects at least one gender
-        document.getElementById("error").innerText = "Please select gender!" //shows error message by replacing the HTML content of id=error
-        document.getElementById("error").style.color = "crimson"
-        document.getElementById("error").style.paddingLeft = "40%"
-    }
+function output() {
+  /* declaration  and formula part */
+  var year = document.getElementById("year").value;
+  var yy = parseInt(year);
+  var month = document.getElementById("month").value;
+  var mm = parseInt(month);
+  var day = document.getElementById("day").value;
+  var dd = parseInt(day);
+  var cc = (yy - 1) / 100 + 1;
+  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleName = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  /**
+   * formula for the Akan Names...source: From IP2 question
+   */
+  var result =
+      parseInt(cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) %
+      7;
+
+  /**
+   * Arithimetic Part...where the magic happens
+   */
+
+  if (document.getElementById("gender").checked) {
+      var gender = "male";
+  } else {
+      var gender = "female";
   }
-  alert(DOB);
+  if (mm < 1 || mm > 12 || (mm == 2 && dd > 29)) {
+      alert("invalid month");
+  } else if (dd < 1 || dd > 31) {
+      alert("invalid date");
+  } else if (Math.round(result) == 1 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Monday ,your akan name is " + maleName[1];
+  } else if (Math.round(result) == 2 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Tuesday ,your akan name is " + maleName[2];
+  } else if (Math.round(result) == 3 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Wednesday ,your akan name is " + maleName[3];
+  } else if (Math.round(result) == 4 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Thursday,your akan name is " + maleName[4];
+  } else if (Math.round(result) == 5 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Friday,your akan name is " + maleName[5];
+  } else if (Math.round(result) == 6 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Saturday,your akan name is " + maleName[6];
+  } else if (Math.round(result) == 0 && gender === "male") {
+      document.getElementById("display").innerHTML =
+          "you were born on Sunday,your akan name is " + maleName[0];
+  } else if (Math.round(result) == 1 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on monday ,your akan name is " + femaleName[1];
+  } else if (Math.round(result) == 2 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Tuesday,your akan name is " + femaleName[2];
+  } else if (Math.round(result) == 3 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Wednesday,your akan name is " + femaleName[3];
+  } else if (Math.round(result) == 4 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Thursday,your akan name is " + femaleName[4];
+  } else if (Math.round(result) == 5 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Friday,your akan name is " + femaleName[5];
+  } else if (Math.round(result) == 6 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Saturday,your akan name is " + femaleName[6];
+  } else if (Math.round(result) == 0 && gender === "female") {
+      document.getElementById("display").innerHTML =
+          "you were born on Sunday,your akan name is " + femaleName[0];
+  } else {
+      alert("Fill in all required fields");
+  }
+}
